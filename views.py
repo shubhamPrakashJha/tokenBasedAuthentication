0462d0b4f2715526fe3a3ae352e9d2c56190c151
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-06 02:17:33 +0530

    Modification: Verify password method check both username and TOKEN.

--- views.py
+++ views.py
@@ -17,11 +17,16 @@ app = Flask(__name__)
 
 
 @auth.verify_password
-def verify_password(username, password):
-	user = session.query(User).filter_by(username=username).first()
-	if not user or not user.verify_password(password):
-		return False
+def verify_password(username_or_token, password):
+	user_id = User.verify_auth_token(username_or_token)
+	if user_id:
+		user = session.query(User).filter_by(id=user_id).one()
+	else:
+		user = session.query(User).filter_by(username=username_or_token).first()
+		if not user or not user.verify_password(password):
+			return False
 	g.user = user
+	return True
 
 
 
